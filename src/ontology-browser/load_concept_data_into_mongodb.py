1a2dddc3e086483b530e116db9e221b2d8bbd716
Russell Poldrack <poldrack@gmail.com>
2024-11-20 08:09:02 -0800

    initial add

---
+++ src/ontology-browser/load_concept_data_into_mongodb.py
@@ -0,0 +1,32 @@
+from pymongo import MongoClient
+import json
+from mongodb_utils import (
+    load_jsonl_to_mongodb
+)
+
+
+def parse_jsonl_line(line):
+    """Parses a single line of JSONL into a dictionary."""
+    json_data = json.loads(line.strip())
+
+    content = json_data['response']['body']['choices'][0]['message']['content']
+    # clean up content
+    content = content.replace("```json", "").replace("```", "")
+    content_dict = json.loads(content)
+    assert content_dict['type'] != 'other', f"type is other for {json_data['custom_id']}"
+    content_dict['model'] = json_data['response']['body']['model']
+    content_dict['custom_id'] = json_data['custom_id'].replace('task-','').replace('/','_')
+    content_dict['system_fingerprint'] = json_data['response']['body']['system_fingerprint']
+    return content_dict
+
+
+if __name__ == "__main__":
+    # Example usage
+    jsonl_path = (
+        "/Users/poldrack/Dropbox/data/ontology-learner/data/concept_results/"
+        "batch_673d2908c74c81908932e5e6769a3c55.jsonl"
+    )
+    database_name = "ontology"
+    collection_name = "ontology-concept"
+    
+    load_jsonl_to_mongodb(jsonl_path, database_name, collection_name, parse_jsonl_line)
